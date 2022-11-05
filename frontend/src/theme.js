b8fa6f11dcb82031b40b09e322e591e87a6c080f
Diana Elena Vasiliu <diana.vasiliu10@gmail.com>
2022-11-05 19:23:29 +0200

    Added color theme

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,10 +1,26 @@
 import {extendTheme} from '@chakra-ui/react';
+import COLORS from './utils/colors';
 
 const theme = extendTheme({
   fonts: {
     body: 'Raleway, sans-serif',
     heading: 'Raleway, sans-serif',
     mono: 'Raleway, sans-serif',
+  },
+  colors: {
+    primary: {
+      300: COLORS.primary,
+      500: COLORS.primaryDark,
+    },
+    secondary: {
+      300: COLORS.secondary,
+    },
+    white: {
+      300: COLORS.white,
+    },
+    black: {
+      300: COLORS.black,
+    },
   },
 });
 
