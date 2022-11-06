f35524a982c5bf8ebf00c6198dcf33189c7e5ebe
Bogdan-Strat <bodo.pa2001@yahoo.com>
2022-11-06 02:36:44 +0200

    Recruitment Management

---
+++ backend/Backend.WebApp/Controllers/RecruiterController.cs
@@ -0,0 +1,43 @@
+﻿using Backend.BusinessLogic.Implementation.Recruiters;
+using Backend.BusinessLogic.Implementation.Recruiters.Models;
+using Backend.WebApp.Code.Base;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+      public class RecruiterController : BaseController
+    {
+        private readonly RecruiterService Service;
+        public RecruiterController(ControllerDependencies dependencies, RecruiterService service) : base(dependencies)
+        {
+            Service = service;
+        }
+
+        [HttpPost("addRecruiter")]
+        public async Task<IActionResult> AddRecruiter([FromBody] AddRecruiterModel model)
+        {
+            await Service.AddRecruiter(model);
+
+            return Ok();
+        }
+
+        [HttpPost("changePassword")]
+        public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordModel model)
+        {
+            await Service.UpdatePassword(model);
+
+            return Ok();
+        }
+
+        [HttpPost("getRecruiters")]
+        public async Task<IActionResult> GetRecruiters([FromBody] Guid currentUserId)
+        {
+            var recruiters = await Service.GetRecruiters(currentUserId);
+
+            return Ok(recruiters);
+        }
+    }
+}
